538b810d1dbc72bae6a69b84619e7bc4d3c868b0
5h4d0w3k <gorski@outlook.sa>
2026-06-18 18:21:36 +0200

    create gui boilerplate

--- XIVHarmonic/Windows/MainWindow.cs
+++ XIVHarmonic/Windows/MainWindow.cs
@@ -13,11 +13,11 @@ public class MainWindow : Window, IDisposable
     private readonly Plugin plugin;
 
     public MainWindow(Plugin plugin)
-        : base("Harmonic Configuration", ImGuiWindowFlags.NoScrollbar | ImGuiWindowFlags.NoScrollWithMouse)
+        : base("Harmonic Configuration")
     {
         SizeConstraints = new WindowSizeConstraints
         {
-            MinimumSize = new Vector2(375, 330),
+            MinimumSize = new Vector2(600, 400),
             MaximumSize = new Vector2(float.MaxValue, float.MaxValue)
         };
 
@@ -25,25 +25,71 @@ public class MainWindow : Window, IDisposable
     }
 
     public void Dispose() { }
+    
+    private static string _conditionString = string.Empty;
 
+    private static bool _checkbox1;
+    private static bool _checkbox2;
+    private static bool _checkbox3;
+    
     public override void Draw()
     {
-        if (OrchestrionIpc.IsAvailable())
+        if (ImGui.BeginTabBar("HarmonicTabs"))
         {
-            if (ImGui.Button("IPC test: Play"))
+            // Active conditions tab
+            if (ImGui.BeginTabItem("Active conditions"))
             {
-                OrchestrionIpc.Play(66);
+                if (ImGui.BeginTable("EntriesTable", 3,
+                    ImGuiTableFlags.Borders | ImGuiTableFlags.RowBg))
+                {
+                    ImGui.TableSetupColumn("Condition", ImGuiTableColumnFlags.WidthStretch, 0.6f);
+                    ImGui.TableSetupColumn("Action");
+                    ImGui.TableSetupColumn("Status", ImGuiTableColumnFlags.WidthFixed, 100);
+                    ImGui.TableHeadersRow();
+
+                    for (int i = 0; i < 33; i++)
+                    {
+                        ImGui.TableNextRow();
+
+                        ImGui.TableSetColumnIndex(0);
+                        ImGui.Text($"If weather is: Rain\nIf area is: gdfklgjdfg{i}\nIf status is: In battle");
+
+                        ImGui.TableSetColumnIndex(1);
+                        ImGui.Text("Play track #66 (Bee My Honey)\nStop playing if conditions not met");
+
+                        ImGui.TableSetColumnIndex(2);
+                        ImGui.Button("Delete");
+                    }
+
+                    ImGui.EndTable();
+                }
+
+                ImGui.EndTabItem();
             }
-            if (ImGui.Button("IPC test: Stop"))
+
+            if (ImGui.BeginTabItem("New condition"))
             {
-                OrchestrionIpc.Play(0);
+                // todo
+                ImGui.InputText("Condition string", ref _conditionString, 256);
+                if (ImGui.Button("Save"))
+                {
+                }
+
+                ImGui.EndTabItem();
             }
+
+            // Configuration tab
+            if (ImGui.BeginTabItem("Configuration"))
+            {
+                // todo
+                ImGui.Checkbox("Checkbox 1", ref _checkbox1);
+                ImGui.Checkbox("Checkbox 2", ref _checkbox2);
+                ImGui.Checkbox("Checkbox 3", ref _checkbox3);
+
+                ImGui.EndTabItem();
+            }
+
+            ImGui.EndTabBar();
         }
-        else
-        {
-            ImGui.Text("Orchestrion not available");
-        }
-        
-        ImGui.Text($"The random config bool is {plugin.Configuration.SomePropertyToBeSavedAndWithADefault}");
     }
 }
